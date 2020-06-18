3b4a28d8f9e2485e86455314e81dd2615c6ff7b5
neeoon <njuarez@hermescorp.io>
2020-06-18 05:01:30 -0500

    [Add] Get distance between all points

--- src/components/map.vue
+++ src/components/map.vue
@@ -9,7 +9,7 @@
                     Localidades
                 </v-row>
                 <v-row class="Bold">
-                    <v-btn @click="reloadPage" style="font-size: 9px" max-width="100%">Generar nuevos almacenes</v-btn>
+                    <v-btn @click="reloadPage" style="font-size: 9px" width="100%">Generar nuevos almacenes</v-btn>
                 </v-row>
             </v-col>
             <v-col class="pa-0" style="height: 100%" >
@@ -49,51 +49,96 @@ export default {
         var user = L.icon({
             iconUrl: 'https://image.flaticon.com/icons/png/512/1946/1946401.png',
             iconSize:     [35, 38], // size of the icon
-            iconAnchor:   [22, 35], // point of the icon which will correspond to marker's location
-            // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            iconAnchor:   [18, 10], // point of the icon which will correspond to marker's location
         });
+
+        var storeLocations = 
+        [
+            {lat: 21.933769, long: -102.304644},
+            {lat: 21.924001, long: -102.289873},
+            {lat: 21.916714, long: -102.312813},
+            {lat: 21.924602, long: -102.318030},
+            {lat: 21.924425, long: -102.306345},
+        ]
 
         var marker, marker2, marker3, marker4, marker5; 
         var geocodeService = L.esri.Geocoding.geocodeService();
 
-        marker  = L.marker([21.933769, -102.304644]).addTo(mymap).on('click', onClick);
+        marker  = L.marker([storeLocations[0].lat, storeLocations[0].long]).addTo(mymap).on('click', onClick);
         marker.bindPopup("<b>Plaza San Telmo</b>");
         
-        marker2  = L.marker([21.924001, -102.289873]).addTo(mymap).on('click', onClick);
+        marker2  = L.marker([storeLocations[1].lat, storeLocations[1].long]).addTo(mymap).on('click', onClick);
         marker2.bindPopup("<b>Centro Comercial Altaria</b>");
 
-        marker3  = L.marker([21.916714, -102.312813]).addTo(mymap).on('click', onClick);
+        marker3  = L.marker([storeLocations[2].lat, storeLocations[2].long]).addTo(mymap).on('click', onClick);
         marker3.bindPopup("<b>Sierra Morena</b>");
 
-        marker4  = L.marker([21.924602, -102.318030]).addTo(mymap).on('click', onClick);
+        marker4  = L.marker([storeLocations[3].lat, storeLocations[3].long]).addTo(mymap).on('click', onClick);
         marker4.bindPopup("<b>Campestre</b>");
 
-        marker5  = L.marker([21.924425, -102.306345]).addTo(mymap).on('click', onClick);
+        marker5  = L.marker([storeLocations[4].lat, storeLocations[4].long]).addTo(mymap).on('click', onClick);
         marker5.bindPopup("<b>Colosio</b>");
 
+        
+        var storageLocations = 
+        [
+            {lat: randomLat(), long: randomLong()},
+            {lat: randomLat(), long: randomLong()},
+            {lat: randomLat(), long: randomLong()},
+            {lat: randomLat(), long: randomLong()},
+            {lat: randomLat(), long: randomLong()},
+        ]
+        
         var almacen1, almacen2, almacen3, almacen4, almacen5;
-        almacen1  = L.marker([randomLat(), randomLong()], {icon: user}).addTo(mymap).on('click', onClick);
+        almacen1  = L.marker([storageLocations[0].lat, storageLocations[0].long], {icon: user}).addTo(mymap).on('click', onClick);
         almacen1.bindPopup("<b>Almacen 1</b>");
 
-        almacen2  = L.marker([randomLat(), randomLong()], {icon: user}).addTo(mymap).on('click', onClick);
+        almacen2  = L.marker([storageLocations[1].lat, storageLocations[1].long], {icon: user}).addTo(mymap).on('click', onClick);
         almacen2.bindPopup("<b>Almacen 2</b>");
 
-        almacen3  = L.marker([randomLat(), randomLong()], {icon: user}).addTo(mymap).on('click', onClick);
+        almacen3  = L.marker([storageLocations[2].lat, storageLocations[2].long], {icon: user}).addTo(mymap).on('click', onClick);
         almacen3.bindPopup("<b>Almacen 3</b>");
 
-        almacen4  = L.marker([randomLat(), randomLong()], {icon: user}).addTo(mymap).on('click', onClick);
+        almacen4  = L.marker([storageLocations[3].lat, storageLocations[3].long], {icon: user}).addTo(mymap).on('click', onClick);
         almacen4.bindPopup("<b>Almacen 4</b>");
 
-        almacen5  = L.marker([randomLat(), randomLong()], {icon: user}).addTo(mymap).on('click', onClick);
-        almacen5.bindPopup("<b>Almacen 5</b>");   
+        almacen5  = L.marker([storageLocations[4].lat, storageLocations[4].long], {icon: user}).addTo(mymap).on('click', onClick);
+        almacen5.bindPopup("<b>Almacen 5</b>"); 
         
-        createStorages();
+        var distances = []
+        
+        for(var i = 0; i < storeLocations.length; i++)
+        {
+            distances[i] = new Array(storageLocations.length);
+            for(var j = 0; j < storageLocations.length; j++ )
+            {
+                distances[i][j] = distance(storeLocations[i].lat, storeLocations[i].long, storageLocations[j].lat, storageLocations[j].long, 'K' )
+            }
+        }
+        console.log(distances)
+        console.log("KM", distance(storeLocations[0].lat, storeLocations[0].long, storageLocations[0].lat, storageLocations[0].long, 'K' ));
 
-        // location  = L.marker([21.922769, -102.301567], {icon: user}).addTo(mymap);
-        // location.openPopup();
 
-        function createStorages() {
-            console.log("Clicked!");
+        function distance(lat1, lon1, lat2, lon2, unit) {
+            if ((lat1 == lat2) && (lon1 == lon2)) {
+                return 0;
+            }
+            else {
+                var radlat1 = Math.PI * lat1/180;
+                var radlat2 = Math.PI * lat2/180;
+                var theta = lon1-lon2;
+                var radtheta = Math.PI * theta/180;
+                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+                if (dist > 1) {
+                    dist = 1;
+                }
+                dist = Math.acos(dist);
+                dist = dist * 180/Math.PI;
+                dist = dist * 60 * 1.1515;
+                if (unit=="K") { dist = dist * 1.609344 }
+                if (unit=="N") { dist = dist * 0.8684 }
+                return parseFloat(dist.toFixed(2));
+            }
         }
 
         function randomLat() {
